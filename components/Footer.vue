02f4acd869e00687742e940f47ceda53e69ef1f8
Dmitry Vasilevsky <dmitry.vasilevsky@immelman.ru>
2021-05-22 00:03:51 +0300

    Добавлен компонент футера

---
+++ components/Footer.vue
@@ -0,0 +1,11 @@
+<template lang="pug">
+  v-footer(padless)
+      v-col.text-center(cols="12") © Uppercase — 2021
+</template>
+
+<style lang="scss" scoped>
+.v-footer {
+  color: $primarydark;
+  background-color: $primarylight;
+}
+</style>
