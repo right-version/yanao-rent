3c391b7e5d88a08ceec3172ec972189ef455d101
Dmitry Vasilevsky <dmitry.vasilevsky@immelman.ru>
2021-05-23 01:20:19 +0300

    Добавлена информация о прокате в продукте

--- pages/product/_id.vue
+++ pages/product/_id.vue
@@ -1,29 +1,49 @@
 <template lang="pug">
-v-container
   .product-page
-    v-row.mt-4
-      v-col(cols=5)
-        client-only
-          lightbox.img(css="h-200 h-lg-250" :cells="1" :items="['https://vktrpnzrv.fvds.ru' + product.photo]")
-  
-      v-col.spb.mt-4(cols=7)
-        .topchick.ml-4
-          h2 {{ product.title || 'Название товара' }}
-          .subtitle {{ product.category.name }} 
-            strong {{ product.distributor.title }}
-          .mt-4.fprice(v-html="formatedPrice")
-          .mt-4 Залог: 
-            span.pledge &nbsp;{{ product.pledge }} ₽
+    v-container
+      v-row.mt-4
+        v-col(cols=5)
+          client-only
+            lightbox.img(css="h-200 h-lg-250" :cells="1" :items="['https://vktrpnzrv.fvds.ru' + product.photo]")
+    
+        v-col.spb.mt-4(cols=7)
+          .topchick.ml-4
+            h2 {{ product.title || 'Название товара' }}
+            .subtitle {{ product.category.name }} 
+            .mt-4.fprice(v-html="formatedPrice")
+            .mt-4 Залог: 
+              span.pledge &nbsp;{{ product.pledge }} ₽
 
 
-        .d-flex.mt-4.button.ml-4
-          RentModal(
-            :id="product.id"
-            :price="product.price"
-          )
+          .d-flex.mt-4.button.ml-4
+            RentModal(
+              :id="product.id"
+              :price="product.price"
+            )
 
-    h1.mt-4 Описание
-    div.mt-2.desc {{ product.description || 'Описание товара' }}
+      h1.mt-4 Описание
+      div.mt-2.desc {{ product.description || 'Описание товара' }}
+
+    .banner
+      v-container
+        h1 Иформация о прокате
+        .d-flex
+          v-avatar.absol.ma-3(size="200")
+            v-img(:src="'https://vktrpnzrv.fvds.ru' +product.distributor.photo")
+
+          .d-flex.dist-card-b.mt-6.ml-6
+            div
+              h3.text-h5(v-text="product.distributor.title")
+              p {{ product.distributor.description }}
+              p {{ address }}
+              p
+                a(:href="product.distributor.url" target="_blank" ) {{ product.distributor.url }}
+              p
+                a(:href="'tel:' + product.distributor.phone" ) {{ product.distributor.phone }}
+
+    v-container
+      h1 Отзывы
+
 
 </template>
 
@@ -42,6 +62,14 @@ export default {
     formatedPrice() {
       const time = [this.product.min_hours, this.product.min_minutes].join(':')
       return formatPrice(this.product.price, time)
+    },
+    address() {
+      let x = ''
+      try {
+        x = JSON.parse(this.product.distributor.coordinate)
+      } catch (error) {}
+
+      return x.address
     },
   },
 }
@@ -79,6 +107,18 @@ export default {
     display: flex;
     flex-direction: column;
     justify-content: space-between;
+  }
+
+  .banner {
+    margin: 30px 0;
+    width: 100%;
+    background: $secondary;
+    padding: 30px 0;
+    color: white;
+  }
+
+  a {
+    text-decoration: underline !important;
   }
 }
 </style>
